9dd4b0bf16ed9de29f6cdaba0af6ed7bfcb2bdc2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-10-27 10:36:58 +0000

    add implementation

---
+++ src/XpWhichLib/ExecutableFileInstancesLocator.cs
@@ -0,0 +1,118 @@
+﻿/*
+    XpWhereLib
+    Copyright (c) 2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+
+using XpWhichLib.Abstractions;
+
+namespace XpWhichLib;
+
+/// <summary>
+/// Represents a class that provides functionality to locate instances of executable files
+/// across multiple drives, directories, and files in a system.
+/// </summary>
+public class ExecutableFileInstancesLocator : IExecutableFileInstancesLocator
+{
+    private readonly IExecutableFileDetector _executableFileDetector;
+
+    /// <summary>
+    /// Provides functionality for locating instances of executable files across drives, directories, and files.
+    /// </summary>
+    public ExecutableFileInstancesLocator(IExecutableFileDetector executableDetector)
+    {
+        _executableFileDetector = executableDetector;
+    }
+
+    /// <summary>
+    /// Locates all instances of the specified executable file across all available drives on the system.
+    /// </summary>
+    /// <param name="executableName">The name of the executable file to be located.</param>
+    /// <returns>An array of <see cref="FileInfo"/> objects representing the located executable file instances.</returns>
+    [SupportedOSPlatform("windows")]
+    [SupportedOSPlatform("macos")]
+    [SupportedOSPlatform("linux")]
+    public FileInfo[] LocateExecutableInstances(string executableName)
+    {
+        ConcurrentBag<FileInfo> concurrentBag = new();
+        
+        IEnumerable<DriveInfo> drives = DriveInfo.GetDrives()
+            .Where(x => x.IsReady);
+
+        Parallel.ForEach(drives, drive =>
+        {
+            foreach (FileInfo fileInfo in LocateExecutableInstancesWithinDrive(drive, executableName))
+            {
+                concurrentBag.Add(fileInfo);
+            }
+        });
+        
+        return concurrentBag.ToArray();
+    }
+
+    /// <summary>
+    /// Locates all instances of the specified executable file within a specific drive on the system.
+    /// </summary>
+    /// <param name="driveInfo">The drive on which to search for the executable file instances.</param>
+    /// <param name="executableName">The name of the executable file to be located.</param>
+    /// <returns>An array of <see cref="FileInfo"/> objects representing the located executable file instances within the specified drive.</returns>
+    public FileInfo[] LocateExecutableInstancesWithinDrive(DriveInfo driveInfo,
+        string executableName)
+    {
+        ConcurrentBag<FileInfo> results = new ConcurrentBag<FileInfo>();
+        
+        string rootDir = driveInfo.RootDirectory.FullName;
+        
+        DirectoryInfo directoryInfo =  new DirectoryInfo(rootDir);
+
+        IEnumerable<DirectoryInfo> directories = directoryInfo.EnumerateDirectories("*", SearchOption.AllDirectories);
+
+        Parallel.ForEach(directories, directory =>
+        {
+            FileInfo[] files =  LocateExecutableInstancesWithinDirectory(directory, executableName);
+
+            foreach (FileInfo file in files)
+            {
+                results.Add(file);
+            }
+        });
+
+        return results.ToArray();
+    }
+
+    /// <summary>
+    /// Locates instances of an executable file within the specified directory.
+    /// </summary>
+    /// <param name="directory">The directory where the search will be conducted.</param>
+    /// <param name="executableName">The name of the executable file to search for.</param>
+    /// <returns>An array of <see cref="FileInfo"/> objects representing the located executable files within the directory.</returns>
+    public FileInfo[] LocateExecutableInstancesWithinDirectory(DirectoryInfo directory,
+        string executableName)
+    {
+        ConcurrentBag<FileInfo> results = new();
+        
+        IEnumerable<FileInfo> files = directory
+            .EnumerateFiles("*", SearchOption.AllDirectories)
+            .Where(file => _executableFileDetector.IsFileExecutable(file));
+
+        Parallel.ForEach(files, file =>
+        {
+            if (file.Exists && (file.Name.Equals(executableName) || file.FullName.Equals(executableName)))
+            { 
+                results.Add(file);
+            }
+        });
+
+        return results.ToArray();
+    }
+}
---
+++ src/XpWhichLib/MultiExecutableLocator.cs
@@ -0,0 +1,98 @@
+﻿/*
+    XpWhereLib
+    Copyright (c) 2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+
+using XpWhichLib.Abstractions;
+
+namespace XpWhichLib;
+
+/// <summary>
+/// Represents a locator that identifies all executable files within specified directories or drives.
+/// Implements the <see cref="IMultiExecutableLocator"/> interface to provide functionality for locating executables.
+/// </summary>
+public class MultiExecutableLocator : IMultiExecutableLocator
+{
+    private readonly IExecutableFileDetector _executableFileDetector;
+
+    /// <summary>
+    /// Represents a locator for identifying all executable files within specified directories or drives.
+    /// </summary>
+    public MultiExecutableLocator(IExecutableFileDetector executableFileDetector)
+    {
+        _executableFileDetector = executableFileDetector;
+    }
+
+    /// <summary>
+    /// Locates all executable files within the specified directory and its subdirectories.
+    /// </summary>
+    /// <param name="directory">The directory to search for executables.</param>
+    /// <returns>An array of <see cref="FileInfo"/> objects representing the executable files found.</returns>
+    /// <exception cref="DirectoryNotFoundException">Thrown when the specified directory does not exist.</exception>
+    [SupportedOSPlatform("windows")]
+    [SupportedOSPlatform("macos")]
+    [SupportedOSPlatform("linux")]
+    [SupportedOSPlatform("freebsd")]
+    public FileInfo[] LocateAllExecutablesWithinDirectory(DirectoryInfo directory)
+    {
+        ConcurrentBag<FileInfo> results = new();
+        
+        IEnumerable<DirectoryInfo> directories = new DirectoryInfo(directory.FullName)
+            .EnumerateDirectories("*",  SearchOption.AllDirectories);
+
+        Parallel.ForEach(directories, dir =>
+        {
+            IEnumerable<FileInfo> files = new DirectoryInfo(dir.FullName)
+                .EnumerateFiles("*", SearchOption.AllDirectories)
+                .Where(file => _executableFileDetector.IsFileExecutable(file));
+            
+            foreach (FileInfo file in files)
+            {
+                results.Add(file);
+            }
+        });
+        
+        return results.ToArray();
+    }
+
+    /// <summary>
+    /// Identifies all executable files within the specified drive by recursively searching through all directories.
+    /// </summary>
+    /// <param name="driveInfo">The drive to search within for executable files.</param>
+    /// <returns>An array of FileInfo objects representing executable files found within the drive.</returns>
+    [SupportedOSPlatform("windows")]
+    [SupportedOSPlatform("macos")]
+    [SupportedOSPlatform("linux")]
+    [SupportedOSPlatform("freebsd")]
+    public FileInfo[] LocateAllExecutablesWithinDrive(DriveInfo driveInfo)
+    {
+        ConcurrentBag<FileInfo> results = new();
+        
+        IEnumerable<DirectoryInfo> directories = new DirectoryInfo(driveInfo.RootDirectory.FullName)
+            .EnumerateDirectories("*",  SearchOption.AllDirectories);
+
+        Parallel.ForEach(directories, directory =>
+        {
+            FileInfo[] files = LocateAllExecutablesWithinDirectory(directory);
+
+            foreach (FileInfo file in files)
+            {
+                results.Add(file);
+            }
+        });
+        
+        return results.ToArray();
+    }
+}
