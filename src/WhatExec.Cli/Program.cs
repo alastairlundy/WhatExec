d7313d7ac4db592ae0ac0f106948c1f68bbc3f0f
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-11-11 17:45:39 +0000

    improve CLI program.cs

--- src/WhatExec.Cli/Program.cs
+++ src/WhatExec.Cli/Program.cs
@@ -1,46 +1,44 @@
 ﻿using System;
 using System.Globalization;
 using AlastairLundy.WhatExec.Cli.Commands;
-using AlastairLundy.WhatExecLib.Abstractions.Detectors;
-using AlastairLundy.WhatExecLib.Abstractions.Locators;
-using AlastairLundy.WhatExecLib.Detectors;
-using AlastairLundy.WhatExecLib.Locators;
 using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console;
 using Spectre.Console.Cli;
 using Spectre.Console.Cli.Extensions.DependencyInjection;
+using WhatExecLib.Extensions.DependencyInjection.DependencyInjection;
 
 IServiceCollection services = new ServiceCollection();
 
-services.AddScoped<IExecutableFileDetector, ExecutableFileDetector>();
-services.AddScoped<IExecutableFileInstancesLocator, IExecutableFileInstancesLocator>();
-services.AddScoped<IMultiExecutableLocator, MultiExecutableLocator>();
+services.AddWhatExecLib(ServiceLifetime.Scoped);
 
 using DependencyInjectionRegistrar registrar = new DependencyInjectionRegistrar(services);
 CommandApp app = new CommandApp(registrar);
+
+FigletText titleText = new FigletText("WhatExec").Centered();
+
+AnsiConsole.Write(titleText);
+Console.WriteLine();
 
 app.Configure(config =>
 {
     config.CaseSensitivity(CaseSensitivity.Commands);
     config.SetApplicationCulture(CultureInfo.CurrentCulture);
-    config.SetApplicationName("xpwhich");
+    config.SetApplicationName("whatexec");
     config.UseAssemblyInformationalVersion();
 
-    Array.ForEach(
-        args,
-        x =>
+    config.AddBranch(
+        "search",
+        conf =>
         {
-            if (x.ToLower().Contains("pretty"))
-                app.SetDefaultCommand<PrettyWhatExecCommand>();
-            else
-            {
-                app.SetDefaultCommand<WhatExecCommand>();
-            }
+            conf.AddCommand<PathOnlySearchCommand>("path");
+
+            conf.AddCommand<DirectoryOnlySearchCommand>("directory").WithAlias("dir");
+
+            conf.AddCommand<DriveOnlySearchCommand>("drive");
+
+            conf.AddCommand<GlobalSearchCommand>("system");
         }
     );
-
-    config.AddCommand<PrettyWhatExecCommand>("pretty");
-
-    config.AddCommand<WhichCompatCommand>("posix").WithAlias("nix");
 });
 
 return app.Run(args);
--- src/WhatExec.Cli/Settings/WhatExecCommandSettings.cs
+++ src/WhatExec.Cli/Settings/WhatExecCommandSettings.cs
@@ -1,22 +1,46 @@
 using System.ComponentModel;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace AlastairLundy.WhatExec.Cli.Settings;
 
-public class WhatExecCommandSettings : CommandSettings
+public abstract class WhatExecBaseCommandSettings : CommandSettings
 {
-    [CommandArgument(0, "<Command(s)>")]
-    public string? Commands { get; init; }
-
-    [CommandOption("--all|-a")]
+    [CommandOption("-a|--all")]
     [DefaultValue(false)]
-    public bool PrintAllResults { get; init; }
+    public bool PrintAllResults { get; set; }
 
-    [CommandOption("--limit|-l")]
-    [DefaultValue(-1)]
+    [CommandOption("-l|--limit")]
+    [DefaultValue(3)]
     public int NumberOfResultsToShow { get; init; }
 
-    [CommandOption("-s")]
+    [CommandOption("--use-caching")]
     [DefaultValue(false)]
-    public bool OnlyReturnExitCode { get; init; }
+    public bool UseCaching { get; init; }
+
+    [CommandOption("--cache-lifetime")]
+    public double? CacheLifetimeMinutes { get; set; }
+
+    [CommandOption("-v|--verbose")]
+    [DefaultValue(false)]
+    public bool ShowErrorsAndBeVerbose { get; init; }
+
+    [CommandOption("--non-interactive")]
+    [DefaultValue(false)]
+    public bool DisableInteractivity { get; init; }
+
+    public override ValidationResult Validate()
+    {
+        if (NumberOfResultsToShow < 0)
+        {
+            ValidationResult.Error(
+                "Number of results to show must be greater than or equal to zero."
+            );
+        }
+
+        if (UseCaching && CacheLifetimeMinutes is null)
+            CacheLifetimeMinutes = 3.0;
+
+        return base.Validate();
+    }
 }
