571267383151f10fed60cad6355dd7924004c072
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-11-19 18:29:26 +0000

    Create GlobalUsings.cs

---
+++ src/WhatExec.Cli/GlobalUsings.cs
@@ -0,0 +1,14 @@
+// Global using directives
+
+global using System;
+global using System.Collections.Generic;
+global using System.ComponentModel;
+global using System.IO;
+global using System.Threading;
+global using AlastairLundy.WhatExec.Cli.Helpers;
+global using AlastairLundy.WhatExec.Cli.Localizations;
+global using AlastairLundy.WhatExec.Cli.Settings;
+global using AlastairLundy.WhatExecLib.Abstractions;
+global using AlastairLundy.WhatExecLib.Caching;
+global using Spectre.Console;
+global using Spectre.Console.Cli;
