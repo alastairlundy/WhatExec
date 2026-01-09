eb87578d5d43aad435de15d70f8f049b2cfee455
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-09 18:25:43 +0000

    Update GlobalUsings.cs

--- src/WhatExec.Cli/GlobalUsings.cs
+++ src/WhatExec.Cli/GlobalUsings.cs
@@ -3,8 +3,10 @@
 global using System;
 global using System.Collections.Generic;
 global using System.ComponentModel;
+global using System.ComponentModel.DataAnnotations;
 global using System.IO;
 global using System.Threading;
+global using DotMake.CommandLine;
 global using Spectre.Console;
 global using WhatExec.Cli.Helpers;
 global using WhatExec.Cli.Localizations;
