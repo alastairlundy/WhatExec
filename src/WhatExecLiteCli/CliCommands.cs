c08ec126742e3a04d05f3d8d68d09aaa6eb32ccc
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-11 12:39:14 +0000

    update WhatExecLite to use updated PathExecutableResolver

--- src/WhatExecLiteCli/CliCommands.cs
+++ src/WhatExecLiteCli/CliCommands.cs
@@ -23,17 +23,16 @@ public class CliCommands
     {
         try
         {
-            IEnumerable<FileInfo> resolvedCommands = ResolveCommands(
-                pathExecutableResolver,
-                commands
-            );
+            bool success = pathExecutableResolver.TryResolveAllExecutables(commands,
+                out IReadOnlyDictionary<string, FileInfo> resolvedExecutables);
+            
 
-            foreach (FileInfo resolvedCommand in resolvedCommands)
+            foreach (FileInfo resolvedCommand in resolvedExecutables.Values)
             {
                 Console.Out.WriteLine(resolvedCommand.FullName);
             }
 
-            return 0;
+            return success ? 0 : 1;
         }
         catch (Exception e)
         {
