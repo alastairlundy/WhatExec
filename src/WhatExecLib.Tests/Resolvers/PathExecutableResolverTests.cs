1e17dbb943b955f4fad135612791c0a99563dbdf
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-27 11:05:30 +0000

    add initial unit testing code

---
+++ src/WhatExecLib.Tests/Resolvers/PathExecutableResolverTests.cs
@@ -0,0 +1,40 @@
+﻿namespace WhatExecLib.Tests.Resolvers;
+
+public class PathExecutableResolverTests
+{
+    private string ProgramFilesDirectory => Environment.GetFolderPath(Environment.Is64BitOperatingSystem
+        ? Environment.SpecialFolder.ProgramFiles
+        : Environment.SpecialFolder.ProgramFilesX86);
+
+    [Test]
+    public async Task Resolve_Dotnet_Path_Executable()
+    {
+        PathExecutableResolver filePathResolver = new PathExecutableResolver();
+
+        FileInfo actual =
+            filePathResolver.ResolveExecutableFilePath(OperatingSystem.IsWindows() ? "dotnet.exe" : "dotnet")
+                .Value;
+        
+        FileInfo expected;
+
+        if (OperatingSystem.IsWindows())
+        {
+            expected = new DirectoryInfo(ProgramFilesDirectory)
+                .EnumerateFiles("*", new EnumerationOptions()
+                {
+                    IgnoreInaccessible = true,
+                    MatchCasing = MatchCasing.CaseInsensitive,
+                    RecurseSubdirectories = true
+                })
+                .First(f => f.Name.Equals("dotnet.exe"));
+        }
+        else
+        {
+            expected = new  FileInfo("/usr/bin/dotnet");
+        }
+
+        
+        await Assert.That(expected.FullName).
+            IsEqualTo(actual.FullName);
+    }
+}
