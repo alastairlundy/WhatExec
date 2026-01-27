ab7c4dccf30646f2438f94fddf33bcdac4fa799f
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-27 23:35:32 +0000

    Update MemoryCachedPathEnvironmentVariableResolver.cs

--- src/WhatExecLib.Caching/Resolvers/MemoryCachedPathEnvironmentVariableResolver.cs
+++ src/WhatExecLib.Caching/Resolvers/MemoryCachedPathEnvironmentVariableResolver.cs
@@ -11,7 +11,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Versioning;
-using DotPrimitives.IO.Paths;
 
 namespace WhatExecLib.Caching.Resolvers;
 
