0f6cc4711b114378935d3b1375e1d6e9b8e69735
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-30 17:42:35 +0000

    Create ExecutableFileResolverTests.cs

---
+++ src/WhatExecLib.Tests/Resolvers/ExecutableFileResolverTests.cs
@@ -0,0 +1,26 @@
+﻿using WhatExecLib.Abstractions;
+using WhatExecLib.Detectors;
+
+namespace WhatExecLib.Tests.Resolvers;
+
+public class ExecutableFileResolverTests
+{
+    private readonly IExecutableFileResolver _executableFileResolver;
+
+    public ExecutableFileResolverTests()
+    {
+        _executableFileResolver = new ExecutableFileResolver(new ExecutableFileDetector(),
+            new PathEnvironmentVariableResolver(new PathEnvironmentVariableDetector()));
+    }
+
+    [Test]
+    public async Task Resolve_VsCode_ExecutableFile()
+    {
+        FileInfo? actual =  _executableFileResolver.LocateExecutable("code.exe", SearchOption.AllDirectories);
+        
+        await Assert.That(actual)
+            .IsNotNull()
+            .And
+            .IsNotEmpty();
+    }
+}
