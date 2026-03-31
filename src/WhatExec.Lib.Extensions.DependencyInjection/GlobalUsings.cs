230461e7b45ed4ce2b7aaa2d01380006322e2a57
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-03-31 16:28:22 +0100

    Create GlobalUsings.cs

---
+++ src/WhatExec.Lib.Extensions.DependencyInjection/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿// Global using directives
+
+global using Microsoft.Extensions.DependencyInjection;
+global using WhatExec.Lib.Abstractions.Detectors;
+global using WhatExec.Lib.Abstractions.Locators;
+global using WhatExec.Lib.Abstractions.Resolvers;
+global using WhatExec.Lib.Detectors;
+global using WhatExec.Lib.Locators;
+global using WhatExec.Lib.Resolvers;
